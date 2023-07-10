1b6dd5d8141794963788531c3d67c093c1e254c8
Sebastian Hoffmann <shoffmann.git@gmail.com>
2023-07-10 18:08:52 +0200

    feat: total overhaul

--- dqn.py
+++ dqn.py
@@ -56,16 +56,16 @@ Action-Space: 3-dimensional vector
     - I.e. accelarations.
 """
 
-POS_BOUNDS = np.array([10, 6.666])
+POS_BOUNDS = torch.tensor([10, 6.666])
 
 def normalize_pos(pos):
-    return pos / POS_BOUNDS
+    return pos / torch.tensor([10, 6.666], dtype=torch.float, device=pos.device)
 
 def normalize_vel(vel):
     return vel / 10
 
 def normalize_angvel(angvel):
-    return np.sign(angvel) * np.log(1 + 0.5*np.fabs(angvel))
+    return torch.sign(angvel) * torch.log(1 + 0.5*torch.abs(angvel))
 
 def normalize_(obs):
     # Player 1
@@ -92,18 +92,54 @@ class ReplayBuffer:
 
     def __init__(self, size):
         self.size = size
-        self.buffer = []
+        self.states_0 = []
+        self.states_1 = []
+        self.actions = []
+        self.rewards = []
+        self.dones = []
         self.position = 0
 
-    def add(self, sample):
-        if len(self.buffer) < self.size:
-            self.buffer.append(None)
-        self.buffer[self.position] = sample
+    def __len__(self):
+        return len(self.states_0)
+    
+    def __getitem__(self, index):
+        return self.states_0[index], self.states_1[index], self.actions[index], self.rewards[index], self.dones[index]
+    
+    def __setitem__(self, index, value):
+        self.states_0[index], self.states_1[index], self.actions[index], self.rewards[index], self.dones[index] = value
+
+    def _extend(self, size=1):
+        self.states_0.extend([None] * size)
+        self.states_1.extend([None] * size)
+        self.actions.extend([None] * size)
+        self.rewards.extend([None] * size)
+        self.dones.extend([None] * size)
+
+    def add(self, state_0, state_1, action, reward, done):
+        if len(self) < self.size:
+            self._extend(1)
+        state_0  = torch.as_tensor(state_0, dtype=torch.float)
+        state_1  = torch.as_tensor(state_1, dtype=torch.float)
+        action   = torch.as_tensor(action, dtype=torch.float)
+        reward   = torch.as_tensor(reward, dtype=torch.float)
+        done     = torch.as_tensor(done, dtype=torch.float)
+        self[self.position] = (state_0, state_1, action, reward, done)
         self.position = (self.position + 1) % self.size
 
-    def sample(self, batch_size):
-        indices = np.random.choice(len(self.buffer), batch_size)
-        return [self.buffer[i] for i in indices]
+    def sample(self, batch_size, device=None):
+        indices = np.random.choice(len(self), batch_size, replace=False)
+        states_0 = torch.stack([self.states_0[idx] for idx in indices])
+        states_1 = torch.stack([self.states_1[idx] for idx in indices])
+        actions = torch.stack([self.actions[idx] for idx in indices])
+        rewards = torch.stack([self.rewards[idx] for idx in indices])
+        dones = torch.stack([self.dones[idx] for idx in indices])
+        if device is not None:
+            states_0 = states_0.to(device, non_blocking=True)
+            states_1 = states_1.to(device, non_blocking=True)
+            actions = actions.to(device, non_blocking=True)
+            rewards = rewards.to(device, non_blocking=True)
+            dones = dones.to(device, non_blocking=True)
+        return states_0, states_1, actions, rewards, dones
 
 class MLPLayer(nn.Module):
     def __init__(self, in_channels, out_channels, activation=nn.ReLU, norm_layer=nn.LayerNorm):
@@ -187,19 +223,29 @@ class NNAgent:
             action = torch.randint(0, 7, ())
         return action.detach().cpu().numpy()
 
-    def _action_to_cont(self, action):
+    def discrete_to_cont(self, action):
         action_cont = [(action == 1) * -1 + (action == 2) * 1,  # player x
                     (action == 3) * -1 + (action == 4) * 1,  # player y
                     (action == 5) * -1 + (action == 6) * 1]  # player angle
         return action_cont
+    
+    def cont_to_discrete(self, action_cont):
+        action = torch.zeros(action_cont.shape[0],1,dtype=torch.long, device=action_cont.device, requires_grad=False)
+        action[action_cont[:, 0] > 0.5] = 1
+        action[action_cont[:, 0] < 0.5] = 2
+        action[action_cont[:, 1] > 0.5] = 3
+        action[action_cont[:, 1] < 0.5] = 4
+        action[action_cont[:, 2] > 0.5] = 5
+        action[action_cont[:, 2] < 0.5] = 6
+        return action
 
     def act(self, obs):
+        obs = torch.as_tensor(obs, dtype=torch.float32, device=self.device)
         obs = normalize_(obs)
-        obs = torch.from_numpy(obs).float().to(self.device)
         Q = self.module(obs)
         action = self._choose_action(Q)
         self.last_action_discrete = action
-        self.last_action = self._action_to_cont(action)
+        self.last_action = self.discrete_to_cont(action)
         self.n += 1
         return self.last_action
 
@@ -337,85 +383,163 @@ def evaluate(agent, game, tracker, action_repeats, opponent=None, N=20):
     plt.savefig('loss.png')
     plt.close()
 
+
+class RLGame:
+    def __init__(self, env, max_steps=500):
+        self.env = env
+        self.max_steps = max_steps
+        self.replay_buffer = ReplayBuffer(200000)
+        self.num_games = -1
+        self.total_steps = 0
+        self.reset()
+
+    @property
+    def action_agent1(self):
+        return self.actions[0]
+    
+    @action_agent1.setter
+    def action_agent1(self, value):
+        self.actions[0] = value
+
+    @property
+    def action_agent2(self):
+        return self.actions[1]
+    
+    @action_agent2.setter
+    def action_agent2(self, value):
+        self.actions[1] = value
+    
+    @property
+    def obs_agent1(self):
+        return self.observations[0]
+    
+    @property
+    def obs_agent2(self):
+        return self.observations[1]
+
+    def reset(self, *args, **kwargs):
+        self.num_games += 1
+        self.cur_step = 0
+        self.observations = [[],[]]
+        self.actions = [None, None]
+        self.reward = 0.0
+
+        obs1, _ = self.env.reset(*args, **kwargs)
+        obs2 = self.env.obs_agent_two()
+        self.obs_agent1.append(obs1)
+        self.obs_agent2.append(obs2)
+
+    def _stack_state(self, observations, horizon):
+        assert len(observations) >= horizon
+        return np.stack(observations[-horizon:]) if horizon > 1 else observations[-1]
+
+    def _get_and_set_action(self, agent_idx, agent, horizon):
+        observations = self.observations[agent_idx]
+        action = self.actions[agent_idx]
+        if action is None:
+            if len(observations) < horizon:
+                action = np.array([0.0,0.0,0.0])
+            else:
+                agent_inp = self._stack_state(observations, horizon)
+                action = agent.act(agent_inp)
+            self.actions[agent_idx] = action
+        return action
+
+    def _reset_actions(self, action_repeats_agent1=1, action_repeats_agent2=1):
+        if self.cur_step % action_repeats_agent1 == 0:
+            self.action_agent1 = None
+        if self.cur_step % action_repeats_agent2 == 0:
+            self.action_agent2 = None
+
+    @torch.no_grad()
+    def step(self, agent1, agent2, action_repeats_agent1=1, action_repeats_agent2=1, horizon_agent1=1, horizon_agent2=1):
+        self._reset_actions(action_repeats_agent1, action_repeats_agent2)
+        action1 = self._get_and_set_action(0, agent1, horizon_agent1)
+        action2 = self._get_and_set_action(1, agent2, horizon_agent2)
+        action = np.hstack([action1, action2])
+        
+        obs_agent1, r, done, _, info = self.env.step(action)
+        obs_agent2 = self.env.obs_agent_two()
+
+        self.obs_agent1.append(obs_agent1)
+        self.obs_agent2.append(obs_agent2)
+
+        state_0 = self._stack_state(self.obs_agent1[:-1], horizon_agent1)
+        state_1 = self._stack_state(self.obs_agent1, horizon_agent1)
+        self.replay_buffer.add(state_0, state_1, action, r, done)
+        self.cur_step += 1
+        self.total_steps += 1
+
+        if done or self.cur_step >= self.max_steps:
+            self.reset()
+            done = True
+        return r, done, info
+
 def DQN():
     BS = 64
     GAMMA = 0.995
     LR = 1e-4
     ACTION_REPEATS = 3
 
+    env = lh.LaserHockeyEnv() 
+    opponent = lh.BasicOpponent()
+    rl_game = RLGame(env)
+
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     tracker = Tracker()
     model = MLP([18, 64, 64, 7]).to(device)
     ema_model = EMAModel(MLP([18, 64, 64, 7]).to(device), model, momentum=0.999)
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
-    replay_buffer = ReplayBuffer(200000)
-
-    env = lh.LaserHockeyEnv()
+    
     agent = NNAgent(model, device, action_repeats=ACTION_REPEATS)
-    opponent = lh.BasicOpponent()
-
-    prepopulate(replay_buffer, ACTION_REPEATS)
-    print(len(replay_buffer.buffer))
+    
+    #prepopulate(replay_buffer, ACTION_REPEATS)
+    #print(len(replay_buffer.buffer))
     
     model.train()
     ema_model.train()
-    for game in range(30000):
-        mode = lh.LaserHockeyEnv.NORMAL
-        obs_agent1, info = env.reset(mode=mode)
-        obs_agent2 = env.obs_agent_two()
-        agent.reset()
-        
+    for game in range(30000):        
         n_updates = 0
         Qs = []
         td_errors = []
-        for i in range(500//ACTION_REPEATS):
-            with torch.no_grad():
-                a1 = agent.act(obs_agent1)
-            a2 = opponent.act(obs_agent2)
-
-            r_cum = -(ACTION_REPEATS/500)
-            for _ in range(ACTION_REPEATS):
-                obs_agent1_new, r, d, _, info = env.step(np.hstack([a1,a2]))
-                obs_agent2_new = env.obs_agent_two()
-                r_cum += r
-                if d: 
-                    break
-            
-            sample = (obs_agent1, agent.last_action_discrete, r_cum, obs_agent1_new, d)
-            replay_buffer.add(sample)
-
-            obs_agent1 = obs_agent1_new
-            obs_agent2 = obs_agent2_new
-
-            if i % 1 == 0:
+        done = False
+        while not done:
+            _, done, _ = rl_game.step(agent, opponent, action_repeats_agent1=ACTION_REPEATS)
+            if len(rl_game.replay_buffer) > 10000 and rl_game.total_steps % 4 == 0:
                 n_updates += 1
                 optimizer.zero_grad()
 
-                batch = replay_buffer.sample(BS)
-                obs, action, r, obs_next, d_ = zip(*batch)
-                obs = normalize(np.array(obs))
-                obs_next = normalize(np.array(obs_next))
-                obs = torch.from_numpy(obs).float().to(device)
-                action = torch.from_numpy(np.array(action)).long().to(device)
-                r = torch.from_numpy(np.array(r)).float().to(device)
-                obs_next = torch.from_numpy(obs_next).float().to(device)
-                d_ = torch.from_numpy(np.array(d_)).float().to(device)
+                state_0, state_1, action, reward, d = rl_game.replay_buffer.sample(BS, device)
+                state_0, state_1 = normalize_(state_0), normalize_(state_1)
+                action = agent.cont_to_discrete(action)
 
-                Q = model(obs)
-                Q_next = ema_model(obs_next)
-                Q_next_max = torch.max(Q_next, dim=1)[0]
-                Q_target = r + GAMMA * Q_next_max * (1 - d_)
+                assert action.shape == (BS, 1)
+                assert reward.shape == (BS,)
+                assert d.shape == (BS,)
 
-                loss = torch.mean((Q_target - Q.gather(1, action.unsqueeze(1))) ** 2)
+                Q = model(state_0)
+                Q_next = ema_model(state_1)
+                Q_next_max = torch.max(Q_next, dim=1, keepdim=True)[0]
+                Q_target = reward[:, None] + GAMMA * Q_next_max * (1 - d[:, None])
+
+                """
+                print('action', action.shape)
+                print('Q', Q.shape)
+                print('Q_next', Q_next.shape)
+                print('Q_next_max', Q_next_max.shape)
+                print('Q_target', Q_target.shape)
+                print('r', reward.shape)
+                print('Qgather', Q.gather(1, action).shape)
+                print('d', d.shape)
+                print('-'*30)"""
+
+                loss = torch.mean((Q_target - Q.gather(1, action)) ** 2)
                 loss.backward()
                 optimizer.step()
                 ema_model.update(model)
 
                 Qs += [Q_target.detach().mean().cpu().item()]
                 td_errors += [loss.detach().cpu().item()]
-
-            if d:
-                break
     
         print(f'Game {game}: {n_updates} updates')
         if n_updates > 0:
@@ -423,7 +547,7 @@ def DQN():
             tracker.add('td_error', np.mean(td_errors), game)
             tracker.add('n_updates', n_updates, game)
     
-        if game % 200 == 0:
+        if game % 200 == 0 and game > 0:
             print('N Steps:', agent.n)
             evaluate(agent, game, tracker, ACTION_REPEATS, opponent=opponent, N=30)
 
