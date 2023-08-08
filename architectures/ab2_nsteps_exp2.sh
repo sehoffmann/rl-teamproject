19c1bcfc05f8db54fb27166016a8c843c06d6394
Sebastian Hoffmann <shoffmann.git@gmail.com>
2023-08-08 16:21:22 +0200

    ablation 2

---
+++ architectures/ab2_nsteps_exp2.sh
@@ -0,0 +1,9 @@
+python ice/train.py \
+    -n "ab2-nsteps2" \
+    --model "lilith" \
+    --schedule "basic" \
+    -f 5000000 \
+    --nsteps 2 \
+    --eps-decay 2_000_000 \
+    --cosine-annealing \
+    --softactions
---
+++ architectures/ab2_nsteps_exp3.sh
@@ -0,0 +1,9 @@
+python ice/train.py \
+    -n "ab2-nsteps3" \
+    --model "lilith" \
+    --schedule "basic" \
+    -f 5000000 \
+    --nsteps 3 \
+    --eps-decay 2_000_000 \
+    --cosine-annealing \
+    --softactions
