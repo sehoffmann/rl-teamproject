87f20b6ada41a007fd425ea8512239b308631bb8
Sebastian Hoffmann <shoffmann.git@gmail.com>
2023-08-09 01:40:50 +0200

    fixed resets

--- ice/deploy_remote.py
+++ ice/deploy_remote.py
@@ -1,4 +1,5 @@
 import argparse
+import torch
 from tournament_client.client.remoteControllerInterface import RemoteControllerInterface
 from dqn import NNAgent
 from numpy import ndarray
@@ -8,14 +9,17 @@ from tournament_client.client.backend.client import Client
 
 
 class RemoteNNAgent(RemoteControllerInterface):
-    def __init__(self, identifier: str, checkpoint_loc: str) -> None:
+    def __init__(self, identifier, checkpoint, device):
         super().__init__(identifier)
-        
-        self.agent = NNAgent.load_model(checkpoint_loc, device='cpu')
+        self.agent = NNAgent.load_model(checkpoint, device=device)
 
     def remote_act(self, obs: ndarray) -> ndarray:
         action = np.array(self.agent.act(obs))
         return action
+    
+    def before_game_starts(self):
+        if hasattr(self.agent, 'reset'):
+            self.agent.reset()
 
 get_log_dir = lambda name: f"tournament_client/logs/{name}"
 
@@ -33,16 +37,17 @@ def create_client(controller: RemoteControllerInterface, args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--checkpoint', type=str)
+    parser.add_argument('checkpoint', type=str)
     parser.add_argument('--identifier', type=str, default='iceq')
     parser.add_argument('--username', type=str, default='user0')
     parser.add_argument('--password', type=str, default='1234')
-    parser.add_argument('--num_games', type=int, default=None)
-
-
+    parser.add_argument('--num-games', type=int, default=None)
+    parser.add_argument('--no-cuda', action='store_true')
     args = parser.parse_args()
 
-    controller = RemoteNNAgent(args.identifier, args.checkpoint)
+    device = 'cuda' if torch.cuda.is_available() and not args.no_cuda else 'cpu'
+
+    controller = RemoteNNAgent(args.identifier, args.checkpoint, device=device)
 
     client = create_client(controller, args)
 
